21371237a7bfa6ba328e0b2e25dc55dff12bf507
isma-91 <isma91@hotmail.it>
2022-11-24 15:09:37 +0100

    First Commit

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HeaderSection/>
+    <MainSection/>
+    <FooterSection/>
+  </div>
+</template>
+
+<script>
+  import HeaderSection from '@/HeaderSection.vue';
+  import MainSection from '@/MainSection.vue';
+  import FooterSection from '@/FooterSection.vue';
+export default {
+
+  components: {
+    HeaderSection,
+    MainSection,
+    FooterSection,
+  },
+};
+</script>
+
+<style>
+
+</style>
