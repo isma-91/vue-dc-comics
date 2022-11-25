7a04aff96336f1807f94850b2dafefef2db12b12
isma-91 <isma91@hotmail.it>
2022-11-25 15:27:28 +0100

    Add imgs grid with array (not working properly)

---
+++ src/components/DcCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img
+      :src="comic.thumb"
+      :alt="comic.series"
+    >
+    <h3> {{ comic.series }} </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DcCard',
+  props: {
+    comic: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
